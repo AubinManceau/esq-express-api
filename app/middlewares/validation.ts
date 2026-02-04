aab98b8a122dbb54ad2c157aa4f4d7ce5440f87a
Aubin MANCEAU <aubin.manceau@wemea.fr>
2026-02-04 17:28:41 +0100

    add zod schemas

--- app/middlewares/validation.ts
+++ app/middlewares/validation.ts
@@ -11,9 +11,9 @@ export function validateData(schema: z.ZodObject<any, any>) {
       const errorMessages = error.issues.map((issue: any) => ({
             message: `${issue.path.join('.')} is ${issue.message}`,
         }))
-        res.status(400).json({ error: 'Invalid data', details: errorMessages });
+        res.status(400).json({ error: 'Données invalides', details: errorMessages });
       } else {
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: 'Erreur interne du serveur' });
       }
     }
   };
